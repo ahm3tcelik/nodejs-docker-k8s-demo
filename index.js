c7661f2e4c87459097f77e546be27fe58ce524c0
Ahmet ÇELİK <37306467+ahm3tcelik@users.noreply.github.com>
2024-09-11 23:38:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/healthcheck', (req, res) => {
+  res.send('SUCCESS')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
